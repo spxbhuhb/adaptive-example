2abb1a9503e1a707d178f2e2a400d9f767c54807
Tóth István Zoltán <tiz@simplexion.hu>
2024-05-20 19:43:41 +0200

    remove ios project

--- shared/src/iosMain/kotlin/main.kt
+++ shared/src/iosMain/kotlin/main.kt
@@ -2,10 +2,8 @@
  * Copyright © 2020-2024, Simplexion, Hungary and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import hu.simplexion.adaptive.ktor.withWebSocketTransport
 import hu.simplexion.adaptive.ui.adapter.ios
-import hu.simplexion.adaptive.ui.basic.text
-import hu.simplexion.adaptive.wireformat.withJson
+import hu.simplexion.adaptive.ui.common.fragment.text
 import platform.UIKit.UIView
 
 fun main(rootView : UIView, trace : Boolean) {
@@ -16,7 +14,7 @@ fun main(rootView : UIView, trace : Boolean) {
 
         ios(rootView, ExampleExports, trace = trace) {
             text("Hello Adaptive 2!")
-            // counter()
+            counter()
         }
     } catch (e : Exception) {
         e.printStackTrace()
